58242a6c67fae06d7d039745efde643ae692b275
pythobotmaker <52012618+pythobotmaker@users.noreply.github.com>
2019-11-02 01:53:59 +0530

    Update zom.py

--- zom.py
+++ zom.py
@@ -1,4 +1,3 @@
-
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -19,6 +18,7 @@ class Scrap_Zomato:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('--ignore-certificate-errors')
         browser = webdriver.Chrome(

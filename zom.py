607852e13f8532a66c9cf71eba471a06e10b883b
pythobotmaker <52012618+pythobotmaker@users.noreply.github.com>
2019-11-02 13:50:58 +0530

    Update zom.py

--- zom.py
+++ zom.py
@@ -15,16 +15,9 @@ class Scrap_Zomato:
         '''
         open driver
         '''
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument('--ignore-certificate-errors')
-        browser = webdriver.Chrome(
-            executable_path=r'/usr/bin/chromedriver',
-            chrome_options = chrome_options
-        )
+        options = Options()
+        options.headless = True
+        browser = webdriver.Chrome(options=options)        
         browser.get(url)
         return browser
 
